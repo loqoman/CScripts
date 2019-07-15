8d2387558e1842fa772586ac7cde49ff1b6e2a55
loqoman <darwin.s.clark@gmail.com>
2019-07-15 15:22:36 -0700

    I suppose some new things?

---
+++ muteable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+const char myword[10] =  "ABCDE";
+
+void main() {
+    printf("Pointer of the first item %p\n",&myword);
+    printf("Value of myword %s\n",myword);
+
+};
+
+void inspectArray(char* root) {
+    fortran
+}
