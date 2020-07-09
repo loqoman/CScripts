939c4cadb3eef957b95ef51385561c434edcdffe
Darwin Clark <darwin.s.clark@gmail.com>
2020-07-08 21:45:31 -0700

    Little work on 2nd advent of code challenge; need more work on syntax to study

--- challenges/adventOfCode2019/2/2.c
+++ challenges/adventOfCode2019/2/2.c
@@ -5,7 +5,7 @@ https://adventofcode.com/2019/day/2
 To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
 
 Input: 
-1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,19,6,23,2,13,23,27,1,9,27,31,2,31,9,35,1,6,35,39,2,10,39,43,1,5,
+1,0,0,3, 1,1,2,3, 1,3,4,3, 1,5,0,3, 2,10,1,19,2,19,6,23,2,13,23,27,1,9,27,31,2,31,9,35,1,6,35,39,2,10,39,43,1,5,
 43,47,1,5,47,51,2,51,6,55,2,10,55,59,1,59,9,63,2,13,63,67,1,10,67,71,1,71,5,75,1,75,6,79,1,10,79,83,1,5,83,87,
 1,5,87,91,2,91,6,95,2,6,95,99,2,10,99,103,1,103,5,107,1,2,107,111,1,6,111,0,99,2,14,0,0
 
@@ -39,15 +39,70 @@ Psudo-code
 
 */
 // Probabbly a good idea to include some library with all the advent of code stuff in it
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ADD_OPCODE 1
+#define MULT_OPCODE 2
+#define STOP_OPCODE 99
 
 void main() {
     FILE *fp;
     FILE *targetWrite;
     // char *charOutTest[5];
-    const char *charOut[5];
-    char i;
-    int out;
-    int numerical;
+
+    const char *rawStrInput[200]; // const = read-only 
+    
+    char *inputTape[200];
+    char *newTape[200];
+
+    fp = fopen("input.txt","r");
+    targetWrite = fopen("output.txt", "w");
+
+    // Getting the input tape from input.txt
+    fgets(rawStrInput, 100000, fp);
+    strcpy(newTape, rawStrInput);
+    printf("%s", &rawStrInput);
+
+    clenseCommas(&rawStrInput, &inputTape);
+}
+/*
+As it pertains to returning strings:
+
+> There are two ways to return strings that won't barf so readily.
+
+>   1. returning buffers (static or dynamically allocated) that live for a while. In C++ use 'helper classes' (for example, std::string) to handle the longevity of data (which requires changing the function's return value), or
+>   2. pass a buffer to the function that gets filled in with information.*
+*/
+
+void clenseCommas(char inputString[], char outputString[]) {
+    int i;
+    char stop = 0;
+
+    for (i = 0; stop = 1; i++) {
+        if (inputString[i] != ',') {
+           strcat(outputString, inputString[i]);
+           printf("%c", inputString[i]);
+        } 
+        // When we catch the error, set stop to 1 
+    }
+}
+
+
+
+void addCommand() {
 
 }
+
+void multCommand() {
+
+}
+
+/*
+
+    printf("Size on input tape is:%lu\n", sizeof(rawStrInput) / sizeof(rawStrInput[0]));
+
+*/
+
+
+
